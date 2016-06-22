c9c5c3d6efad8a3dd7b77c20255571c53387025f
Michael McKnight <michael@mcknight.ninja>
2016-06-22 22:23:50 +1200

    Return default weeks from WeekService. InsertOrUpdate weeks..

--- src/SandwichClub.Api/Services/WeekService.cs
+++ src/SandwichClub.Api/Services/WeekService.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using SandwichClub.Api.Repositories;
 using SandwichClub.Api.Repositories.Models;
 
@@ -7,6 +8,40 @@ namespace SandwichClub.Api.Services
     {
         public WeekService(IWeekRepository weekRepository) : base(weekRepository)
         {
+        }
+
+        public override async Task<Week> GetByIdAsync(int weekId)
+        {
+            var week = await Repository.GetByIdAsync(weekId);
+
+            return week ?? new Week {WeekId = weekId};
+        }
+
+        public override Task<Week> InsertAsync(Week week)
+        {
+            return InsertOrUpdateAsync(week);
+        }
+
+        public override Task UpdateAsync(Week week)
+        {
+            return InsertOrUpdateAsync(week);
+        }
+
+        public async Task<Week> InsertOrUpdateAsync(Week week)
+        {
+            var existingWeek = await Repository.GetByIdAsync(week.WeekId);
+
+            var exists = existingWeek != null;
+            var delete = week.Cost <= 0.0 && week.ShopperUserId == null;
+
+            if (exists && delete)
+                await Repository.DeleteAsync(existingWeek);
+            else if (exists)
+                await Repository.UpdateAsync(week);
+            else if (!delete)
+                week = await Repository.InsertAsync(week);
+
+            return week;
         }
     }
 }
