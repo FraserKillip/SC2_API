1de4bbe80cd98de0e2febb138e3c5639c1e36890
Michael McKnight <michael@mcknight.ninja>
2017-07-01 16:54:58 +1200

    Fix test.

--- test/SandwichClub.Api.Tests/Services/WeekServiceTests.cs
+++ test/SandwichClub.Api.Tests/Services/WeekServiceTests.cs
@@ -120,7 +120,7 @@ namespace SandwichClub.Api.Tests.Services
             public void GetWeekId_GivenStartOfComputerTime_IdShouldBeZero()
             {
                 // When
-                var weekId = Service.GetWeekId(new DateTime(1970, 1, 5));
+                var weekId = Service.GetWeekId(StartOfComputerTime);
 
                 // Verify
                 Assert.Equal(0, weekId);
